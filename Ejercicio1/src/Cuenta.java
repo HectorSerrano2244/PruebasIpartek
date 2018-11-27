867fd1f0dd84c84e015e9382b176ded3cc6fb60e
AjRoBSeYeR <39397248+AjRoBSeYeR@users.noreply.github.com>
2018-11-27 01:07:43 +0100

    Guardado de cuenta en fichero

--- Ejercicio1/src/Cuenta.java
+++ Ejercicio1/src/Cuenta.java
@@ -1,49 +1,88 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Cuenta {
 	private String titular;
 	private Integer cuenta;
 	private Double saldo;
 	private static final Double SALDO_POR_DEFECTO = 0.0;
+	private static final String RUTA_FICHERO_CUENTAS = "C:\\Temp\\cuentas.tmp";
+	private static final String RUTA_FICHEROS = "C:\\Temp";
 
-	public static void recogerDatosCuenta() {
-		Controlscanner preguntar= new Controlscanner();
-		Cuenta nuevacuenta= new Cuenta();
+	public static void recogerDatosCuenta() throws IOException {
+		Controlscanner preguntar = new Controlscanner();
+		Cuenta nuevacuenta = new Cuenta();
 		System.out.println("Introduzca el nombre del titular de la cuenta");
 		nuevacuenta.setTitular(preguntar.getCaptura().nextLine());
 		if (completarYGuardarCuenta(nuevacuenta)) {
 			System.out.println("Cuenta creada");
-		}else {
+			System.out.println(nuevacuenta);
+		} else {
 			throw new RuntimeException("Error al crear la cuenta");
 		}
 	}
-	public static Boolean completarYGuardarCuenta(Cuenta nuevacuenta) {
-		Boolean exito=true;
+
+	public static Boolean completarYGuardarCuenta(Cuenta nuevacuenta) throws IOException {
+		Boolean exito = true;
 		nuevacuenta.setSaldo(SALDO_POR_DEFECTO);
 		nuevacuenta.setCuenta(calculaNumeroCuenta());
+		File fichero = new File(RUTA_FICHERO_CUENTAS);
+		BufferedWriter escritura = new BufferedWriter(new FileWriter(fichero,true));
+		escritura.write(nuevacuenta.toString());
+		escritura.close();
 		return exito;
 	}
-	public static Integer calculaNumeroCuenta() {
-		
-		return 0;
+
+	public static Integer calculaNumeroCuenta() throws IOException {
+		File ruta = new  File(RUTA_FICHEROS);
+		ruta.mkdirs();
+		File fichero = new File(RUTA_FICHERO_CUENTAS);
+		Integer cuantas=0;
+		if (fichero.exists()) {
+			String lineafichero;
+			BufferedReader lectura = new BufferedReader(new FileReader(fichero));
+			while ((lineafichero = lectura.readLine()) != null) {
+				cuantas++;
+			}
+			lectura.close();
+			return cuantas+=1;
+		} else {
+			BufferedWriter escritura = new BufferedWriter(new FileWriter(fichero));
+			escritura.write("");
+			escritura.newLine();
+			escritura.close();
+			return 1;
+		}	
 	}
+
 	public String getTitular() {
 		return titular;
 	}
+
 	public void setTitular(String titular) {
 		this.titular = titular;
 	}
+
 	public Integer getCuenta() {
 		return cuenta;
 	}
+
 	public void setCuenta(Integer cuenta) {
 		this.cuenta = cuenta;
 	}
+
 	public Double getSaldo() {
 		return saldo;
 	}
+
 	public void setSaldo(Double saldo) {
 		this.saldo = saldo;
 	}
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -53,6 +92,7 @@ public class Cuenta {
 		result = prime * result + ((titular == null) ? 0 : titular.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -79,9 +119,10 @@ public class Cuenta {
 			return false;
 		return true;
 	}
+
 	@Override
 	public String toString() {
-		return "Cuenta [titular=" + titular + ", cuenta=" + cuenta + ", saldo=" + saldo + "]";
+		return "[titular=" + titular + ", cuenta=" + cuenta + ", saldo=" + saldo + "]";
 	}
-	
+
 }
--- Ejercicio1/src/Pral.java
+++ Ejercicio1/src/Pral.java
@@ -1,7 +1,8 @@
+import java.io.IOException;
 
 public class Pral {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String opcion="";
 		Controlscanner recepcion= new Controlscanner();
 		do {
