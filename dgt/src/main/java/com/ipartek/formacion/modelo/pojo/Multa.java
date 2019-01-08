7eb2ad697528de61bb5e0ea52690a5578c057d1d
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2019-01-08 10:45:56 +0100

    getter y setter de multa

--- dgt/src/main/java/com/ipartek/formacion/modelo/pojo/Multa.java
+++ dgt/src/main/java/com/ipartek/formacion/modelo/pojo/Multa.java
@@ -1,9 +1,53 @@
 package com.ipartek.formacion.modelo.pojo;
 
+import java.util.Date;
+
 public class Multa {
 	private Long id;
 	private Float importe;
 	private String concepto;
+	private Date fecha;
+	private Agente agente;
+	private Coche coche;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Float getImporte() {
+		return importe;
+	}
+	public void setImporte(Float importe) {
+		this.importe = importe;
+	}
+	public String getConcepto() {
+		return concepto;
+	}
+	public void setConcepto(String concepto) {
+		this.concepto = concepto;
+	}
+	public Date getFecha() {
+		return fecha;
+	}
+	public void setFecha(Date fecha) {
+		this.fecha = fecha;
+	}
+	public Agente getAgente() {
+		return agente;
+	}
+	public void setAgente(Agente agente) {
+		this.agente = agente;
+	}
+	public Coche getCoche() {
+		return coche;
+	}
+	public void setCoche(Coche coche) {
+		this.coche = coche;
+	}
+	
+	
 	
 	
 
