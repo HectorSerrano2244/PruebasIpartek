0c83c6d0d377e5cb2b24147cb3e32b8c2fdee5b5
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2019-01-15 11:35:58 +0100

    cambios finalizados

--- dgt/src/main/java/com/ipartek/formacion/modelo/daos/MultaDAO.java
+++ dgt/src/main/java/com/ipartek/formacion/modelo/daos/MultaDAO.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.sql.Types;
 import java.util.ArrayList;
 
 import org.apache.log4j.Logger;
@@ -26,7 +27,7 @@ public class MultaDAO {
 	private static final String SQL_GETBYID_BAJA = "SELECT m.id, m.fecha_alta, m.fecha_baja, m.importe, m.concepto, c.matricula, modelo, km FROM multa m, coche c WHERE m.id_coche = c.id AND m.id = ?;";
 	private static final String SQL_GETALL_BYUSER = "{call pa_multa_getAll_User(?)}";
 	private static final String SQL_GETALL_BYUSER_BAJA = "SELECT m.id, m.fecha_alta, m.fecha_baja, c.matricula, c.modelo FROM multa m, coche c WHERE m.id_coche = c.id AND m.id_agente = ? AND fecha_baja IS NOT NULL ORDER BY m.id DESC LIMIT 1000;";
-	private static final String SQL_INSERT = "INSERT INTO multa (importe, concepto, id_coche, id_agente) VALUES (?, ?, ?, ?);";
+	private static final String SQL_INSERT = "{call pa_multa_insert(?,?,?,?,?)}";
 	private static final String SQL_UPDATE = "UPDATE multa SET fecha_baja = CURRENT_TIMESTAMP  WHERE id = ?;";
 //	private static final String SQL_UPDATE_RECUPERAR = "UPDATE multa SET fecha_baja = NULL WHERE id = ?;";
 
@@ -46,20 +47,19 @@ public class MultaDAO {
 	public Multa getById(long id, String opm) {
 
 		Multa m = null;
-		
+
 		isGetById = true;
 
 		try (Connection conn = ConnectionManager.getConnection();
 				CallableStatement cs = conn.prepareCall(("baja".equals(opm)) ? SQL_GETBYID_BAJA : SQL_GETBYID);) {
 			cs.setLong(1, id);
-			
+
 			if ("baja".equals(opm)) {
 				isBaja = true;
-			}
-			else {
+			} else {
 				isBaja = false;
 			}
-			
+
 			try (ResultSet rs = cs.executeQuery()) {
 				while (rs.next()) {
 					m = rowMapper(rs);
@@ -78,11 +78,11 @@ public class MultaDAO {
 		ArrayList<Multa> multas = new ArrayList<Multa>();
 		isGetById = false;
 		try (Connection conn = ConnectionManager.getConnection();
-				CallableStatement cs = conn.prepareCall(("baja".equals(opm)) ? SQL_GETALL_BYUSER_BAJA : SQL_GETALL_BYUSER);) {
+				CallableStatement cs = conn
+						.prepareCall(("baja".equals(opm)) ? SQL_GETALL_BYUSER_BAJA : SQL_GETALL_BYUSER);) {
 			if ("baja".equals(opm)) {
 				isBaja = true;
-			}
-			else {
+			} else {
 				isBaja = false;
 			}
 			cs.setLong(1, id);
@@ -109,14 +109,16 @@ public class MultaDAO {
 		boolean resul = false;
 		isGetById = false;
 		try (Connection conn = ConnectionManager.getConnection();
-				PreparedStatement pst = conn.prepareStatement(SQL_INSERT);) {
+				CallableStatement cs = conn.prepareCall(SQL_INSERT);) {
 
-			pst.setDouble(1, m.getImporte());
-			pst.setString(2, m.getConcepto());
-			pst.setLong(3, m.getCoche().getId());
-			pst.setLong(4, m.getAgente().getId());
-			int affectedRows = pst.executeUpdate();
+			cs.setDouble(1, m.getImporte());
+			cs.setString(2, m.getConcepto());
+			cs.setLong(3, m.getCoche().getId());
+			cs.setLong(4, m.getAgente().getId());
+			cs.registerOutParameter(5, Types.INTEGER);
+			int affectedRows = cs.executeUpdate();
 			if (affectedRows == 1) {
+				m.setId(cs.getLong(5));
 				resul = true;
 			}
 
@@ -127,12 +129,12 @@ public class MultaDAO {
 
 	public boolean update(Multa m) throws SQLException {
 
-	boolean resul = false;
+		boolean resul = false;
 		try (Connection conn = ConnectionManager.getConnection();
 				PreparedStatement pst = conn.prepareStatement(SQL_UPDATE);) {
 
 			pst.setLong(1, m.getId());
-			
+
 			int affectedRows = pst.executeUpdate();
 			if (affectedRows == 1) {
 				resul = true;
@@ -142,19 +144,18 @@ public class MultaDAO {
 
 	}
 
-
 	private Multa rowMapper(ResultSet rs) throws SQLException {
 		Multa m = new Multa();
 		Coche c = new Coche();
-		Timestamp timestampalta=rs.getTimestamp("fecha_alta");
+		Timestamp timestampalta = rs.getTimestamp("fecha_alta");
 		m.setFechaAlta(new java.util.Date(timestampalta.getTime()));
 		if (isBaja) {
-			Timestamp timestampbaja=rs.getTimestamp("fecha_baja");
+			Timestamp timestampbaja = rs.getTimestamp("fecha_baja");
 			m.setFechaBaja(new java.util.Date(timestampbaja.getTime()));
 		}
 		m.setId(rs.getLong("id"));
 		c.setMatricula(rs.getString("matricula"));
-		if(isGetById) {	
+		if (isGetById) {
 			m.setImporte(rs.getDouble("importe"));
 			m.setConcepto(rs.getString("concepto"));
 			c.setId(rs.getLong("id"));
