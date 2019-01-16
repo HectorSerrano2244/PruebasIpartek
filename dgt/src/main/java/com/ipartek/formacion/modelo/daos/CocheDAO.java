41ce7ad7ef4a0b030920f27277919d1329a17c0c
RaulAbejonDelgado <raul.abejon.delgado@gmail.com>
2019-01-16 12:10:52 +0100

    CocheDAO actualizado con relación a la bbdd

--- dgt/src/main/java/com/ipartek/formacion/modelo/daos/CocheDAO.java
+++ dgt/src/main/java/com/ipartek/formacion/modelo/daos/CocheDAO.java
@@ -14,12 +14,11 @@ import com.ipartek.formacion.modelo.pojo.Coche;
 import com.ipartek.formacion.modelo.pojo.Mensaje;
 
 public class CocheDAO {
-	
+
 	private final static Logger LOG = Logger.getLogger(CocheDAO.class);
 	private static CocheDAO INSTANCE = null;
 	private Mensaje mensaje = null;
-
-	private static final String SQL_GETMATRICULAS = "{call pa_coche_getMatriculas()}";
+	
 	private static final String SQL_GETMATRICULA = "{call pa_coche_getMatricula(?)}";
 
 	// constructor privado, solo acceso por getInstance()
@@ -43,17 +42,22 @@ public class CocheDAO {
 		c.setKm(rs.getInt("km"));
 		return c;
 	}
-	
+
 	public Coche getByMatricula(String mat) {
 		Coche c = null;
-		try (Connection conn = ConnectionManager.getConnection(); CallableStatement cs = conn.prepareCall(SQL_GETMATRICULA);) {
+		try (Connection conn = ConnectionManager.getConnection();
+				CallableStatement cs = conn.prepareCall(SQL_GETMATRICULA);) {
 
 			cs.setString(1, mat);
 
 			try (ResultSet rs = cs.executeQuery()) {
-
-				while (rs.next()) {
-					c = rowMapper(rs);
+				try {
+					while (rs.next()) {
+						c = rowMapper(rs);
+					}
+				} catch (Exception e) {
+					mensaje = new Mensaje(Mensaje.TIPO_WARNING, "No se encontró la matrícula");
+					LOG.warn(mensaje.getTexto(), e);
 				}
 			}
 
@@ -61,26 +65,5 @@ public class CocheDAO {
 			LOG.error(e);
 		}
 		return c;
-	}
-	
-	public ArrayList<Coche> getMatriculas() {
-		ArrayList<Coche> matriculas = new ArrayList<Coche>();
-		try (Connection conn = ConnectionManager.getConnection();
-				CallableStatement cs = conn.prepareCall(SQL_GETMATRICULAS);) {
-
-			try (ResultSet rs = cs.executeQuery()) {
-				while (rs.next()) {
-					try {
-						matriculas.add(rowMapper(rs));
-					} catch (Exception e) {
-						mensaje = new Mensaje(Mensaje.TIPO_WARNING, "No se encontró ninguna matricula");
-						LOG.warn(mensaje.getTexto(), e);
-					}
-				}
-			}
-		} catch (Exception e) {
-			LOG.error(e);
-		}
-		return matriculas;
 	}
 }
