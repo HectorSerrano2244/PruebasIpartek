eb4af6e1452941641fec07e46d034b408aab3f44
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2019-01-23 14:01:12 +0100

    ws fonando

--- dgt/WS_SOAP/src/main/java/com/ipartek/formacion/dgt/wssoap/WSDatosVehiculo.java
+++ dgt/WS_SOAP/src/main/java/com/ipartek/formacion/dgt/wssoap/WSDatosVehiculo.java
@@ -4,14 +4,13 @@ import com.ipartek.formacion.modelo.daos.CocheDAO;
 import com.ipartek.formacion.modelo.pojo.Coche;
 
 public class WSDatosVehiculo {
-	private void ini() {
-		CocheDAO cochedao = CocheDAO.getInstance();
 
-	}
 	public Coche obtenerDatos(String matricula) {
-	
-		Coche c = 
-		
+		CocheDAO cocheDAO = CocheDAO.getInstance();
+		Coche c = cocheDAO.getByMatricula(matricula);
+		if (c == null) {
+			c=new Coche();
+		}
 		return c;
 	}
 }
