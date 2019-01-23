acb14f15e0d719fb00ad2bab945291c0ff726957
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2019-01-23 09:40:26 +0100

    imports

--- dgt/src/main/java/com/ipartek/formacion/modelo/daos/LoginDAO.java
+++ dgt/src/main/java/com/ipartek/formacion/modelo/daos/LoginDAO.java
@@ -2,7 +2,6 @@ package com.ipartek.formacion.modelo.daos;
 
 import java.sql.CallableStatement;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
