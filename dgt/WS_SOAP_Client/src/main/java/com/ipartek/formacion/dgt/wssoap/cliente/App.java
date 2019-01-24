627830b4b4241196b18619da4d85d2d8965dcc2f
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2019-01-24 11:31:21 +0100

    cliente con app

---
+++ dgt/WS_SOAP_Client/src/main/java/com/ipartek/formacion/dgt/wssoap/cliente/App.java
@@ -0,0 +1,15 @@
+package com.ipartek.formacion.dgt.wssoap.cliente;
+
+import java.rmi.RemoteException;
+
+import com.ipartek.formacion.dgt.wssoap.WSDatosVehiculoProxy;
+
+public class App {
+
+	public static void main(String[] args) throws RemoteException {
+		WSDatosVehiculoProxy cliente= new WSDatosVehiculoProxy();
+		cliente.obtenerDatos("3548MKZ");
+
+	}
+
+}
