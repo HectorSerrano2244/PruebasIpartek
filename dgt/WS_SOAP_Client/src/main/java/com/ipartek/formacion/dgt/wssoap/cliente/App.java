9a1d64d40c43ee2cced95a7538c9c16cda796d20
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2019-01-25 08:23:26 +0100

    app java para provar web service SOAP y montaje json-server REST

--- dgt/WS_SOAP_Client/src/main/java/com/ipartek/formacion/dgt/wssoap/cliente/App.java
+++ dgt/WS_SOAP_Client/src/main/java/com/ipartek/formacion/dgt/wssoap/cliente/App.java
@@ -3,13 +3,14 @@ package com.ipartek.formacion.dgt.wssoap.cliente;
 import java.rmi.RemoteException;
 
 import com.ipartek.formacion.dgt.wssoap.WSDatosVehiculoProxy;
+import com.ipartek.formacion.modelo.pojo.Coche;
 
 public class App {
 
 	public static void main(String[] args) throws RemoteException {
 		WSDatosVehiculoProxy cliente= new WSDatosVehiculoProxy();
-		cliente.obtenerDatos("3548MKZ");
-		System.out.println(cliente);
+		Coche coche=cliente.obtenerDatos("3548MKZ");
+		System.out.println(coche);
 	}
 
 }
